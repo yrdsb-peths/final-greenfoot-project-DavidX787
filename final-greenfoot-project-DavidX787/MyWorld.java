a48e3e7a5c8baae13eb0302ece61f4a342241a20
DavidX787 <340868082@gapps.yrdsb.ca>
2023-05-31 08:46:28 -0400

    New World

---
+++ final-greenfoot-project-DavidX787/MyWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author David Xu 
+ * @version 5/31/23
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
